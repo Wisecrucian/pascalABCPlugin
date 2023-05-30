2914c28fae5564b8b40f9f00c3b621ddba75cf22
IDante <maxim.polyanskiy@mail.ru>
2023-05-30 19:12:35 +0300

    test highlight

--- extension.js
+++ extension.js
@@ -13,16 +13,16 @@ function activate(context) {
 
 class PascalABCTokenProvider {
   async provideDocumentSemanticTokens(document) {
-    const syntaxDefinitionPath = "https://github.com/pascalabcnet/pascalabcnet/blob/master/YMC/Highlighting/Light/lex.xshd";
-    const syntaxDefinition = await fs.readFile(syntaxDefinitionPath, { encoding: 'utf-8' });
-    const result = await parser.parseStringPromise(syntaxDefinition);
+    // const syntaxDefinitionPath = "https://github.com/pascalabcnet/pascalabcnet/blob/master/YMC/Highlighting/Light/lex.xshd";
+    // const syntaxDefinition = await fs.readFile(syntaxDefinitionPath, { encoding: 'utf-8' });
+    // const result = await parser.parseStringPromise(syntaxDefinition);
 
-    const colors = result.SyntaxDefinition.Color;
+    // const colors = result.SyntaxDefinition.Color;
 
-    colors.forEach((color) => {
-      const name = color.$.name;
-      const foreground = color.$.foreground;
-    });
+    // colors.forEach((color) => {
+    //   const name = color.$.name;
+    //   const foreground = color.$.foreground;
+    // });
 
     return tokens;
   }
